1b88c740ad34f7d595d2ace1f319ddf81bdc9db8
Huzefa <hdossaji94@gmail.com>
2018-07-03 21:04:53 -0400

    final

--- src/main.cpp
+++ src/main.cpp
@@ -145,8 +145,8 @@ int main() {
           // Find the MPC solution.
           auto vars = mpc.Solve(state, coeffs);
 
-          steer_value = vars[0];
-          throttle_value = vars[1];
+          double steer_value = vars[0];
+          double throttle_value = vars[1];
 
           json msgJson;
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
